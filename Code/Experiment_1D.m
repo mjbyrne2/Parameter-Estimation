606eb0e98a2672276bc9d325477f8c24dde5ec59
Unknown <mjbyrne2@asu.edu>
2018-09-16 17:22:59 -0700

    Added Experiment_1D.m and updated .gitignore
    .gitignore now includes MATLAB autosave files

---
+++ Code/Experiment_1D.m
@@ -0,0 +1,14 @@
+%% Experiment_1D.m
+% Modified version of Jakob Hansen's script
+% test_pointwise_convergence_NOISE.m. 
+%
+% This script runs the one-dimensional numerical experiment which is used
+% to investigate the effects of downsampling on parameter estimation for
+% regularization methods. 
+%
+% Regularization method(s) considered: Tihkonov
+%
+% Companion m-files: 
+% 
+% See UPRE_Test_B3.m as a template.
+%
