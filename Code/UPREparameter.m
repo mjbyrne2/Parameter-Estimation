6699cb57a2d7f1325f8c32c031b773958259739c
Unknown <mjbyrne2@asu.edu>
2018-10-02 20:51:45 -0700

    Added different methods in UPREparameter.m
    The methods are to find the minima of the UPRE functionals. A switch is added to switch between methods.

--- Code/UPREparameter.m
+++ Code/UPREparameter.m
@@ -5,25 +5,44 @@ function [u,lambda] = UPREparameter(dataSpec,operatorSpec,smoothingSpec,...
 %
 % Companion files: UPREfunctional.m
 
-% Generate the UPRE vector:
-u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
-    variance,L,trunc);
+n = length(dataSpec);
+if n < 5
+    test = 0;
+else 
+    test = 1;
+end
+switch test
+    case 0
+        % Generate the UPRE vector:
+        u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
+            variance,L,trunc);
 
-% Now find the (possibily) shallow minimum:
-step = 5;  % Step size for measuring relative increase
-tol = 0.2; % Percent relative increase required 
-v = (u(step:step:end)-u(1:step:end-(step-1)))./u(1:step:end-(step-1));
-l1 = L(step*find(v > tol,1));
+        % Now find the (possibily) shallow minimum:
+        step = 5;  % Step size for measuring relative increase
+        tol = 0.01; % Percent relative increase required 
+        v = (u(step:step:end)-u(1:step:end-(step-1)))./u(1:step:end-(step-1));
+        v = (u(step:step:end)-u(1:step:end-(step-1)))./...
+            (L(step:step:end)-L(1:step:end-(step-1)));
+        l1 = L((step-1)*find(abs(v) > tol,1)+1);
 
-% Original:
-% U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
-%     smoothingSpec,variance,lambda,trunc);
-% lambda = fminbnd(U,1e-6,10);
+        % Original:
+        % U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
+        %     smoothingSpec,variance,lambda,trunc);
+        % lambda = fminbnd(U,1e-6,10);
 
-% Location of minimal component:
-l2 = L(u == min(u));
+        % Location of minimal component:
+        l2 = L(u == min(u));
 
-% Take the largest parameter:
-lambda = max(l1,l2);
+        % Take the largest parameter:
+        lambda = max(l1,l2);
+
+    case 1 
+        u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
+            variance,L,trunc);
+        % Original:
+        U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
+            smoothingSpec,variance,lambda,trunc);
+        lambda = fminbnd(U,1e-6,10);
+end   
 
 end
