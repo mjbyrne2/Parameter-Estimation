0e6360e20738d54517515cbe4bd3c63e309f023f
Unknown <mjbyrne2@asu.edu>
2018-12-13 16:42:04 -0700

    In GaussianBlur_1D.m, switched 'radius' to 'width'

--- Code/GaussianBlur_1D.m
+++ Code/GaussianBlur_1D.m
@@ -1,4 +1,4 @@
-function [x_b,p] = GaussianBlur_1D(t,x,radius) 
+function [x_b,p] = GaussianBlur_1D(t,x,width) 
 % gaussianBlur_1D performs one-dimensional Gaussian blurring on the vector
 % x, using a discretized Gaussian point spread function (PSF) on the 
 % discretized domain t. The vector t is assumed to be a equispaced 
@@ -12,7 +12,7 @@ N = length(t);
 dt = 1/N;
 i = 1:N;
 k = ceil(N/2);  % Center is chosen to be about 1/2
-p = exp(-radius*(dt*(i-k)).^2); % Gaussian PSF
+p = exp(-width*(dt*(i-k)).^2); % Gaussian PSF
 p = p/sum(abs(p));  %   Normalization
 
 x_b = convPeriodic_1D(x,p);
