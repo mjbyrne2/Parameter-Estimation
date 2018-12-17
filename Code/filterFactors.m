65a694af171e67c4db6db66b894602dcd46d2552
Unknown <mjbyrne2@asu.edu>
2018-12-17 13:05:53 -0700

    Changed 'tilde' to 'hat' in filterFactors.m

--- Code/filterFactors.m
+++ Code/filterFactors.m
@@ -1,4 +1,4 @@
-function q = filterFactors(htilde,lambda,dtilde)
+function q = filterFactors(h_hat,lambda,d_hat)
 % filterFactors generates the Tikhonov filter factors used in Tikhonov
 % regularization method. htilde is the discrete Fourier transform (DFT) of
 % h, where h is the discretization of the Gaussian PSF. dtilde is the DFT
@@ -7,14 +7,14 @@ function q = filterFactors(htilde,lambda,dtilde)
 
 % If dtilde is omitted, it is set to a row vector of ones:
 if nargin < 3
-    dtilde = ones(size(length(htilde)));
+    d_hat = ones(size(length(h_hat)));
 end
 
-q = abs(htilde).^2./(abs(htilde).^2 + (lambda^2)*abs(dtilde).^2);
+q = abs(h_hat).^2./(abs(h_hat).^2 + (lambda^2)*abs(d_hat).^2);
 
 % Cleaning up components:
-for i = 1:length(htilde)
-    if htilde(i) == 0
+for i = 1:length(h_hat)
+    if h_hat(i) == 0
         q(i) = 0;
     end
 end
