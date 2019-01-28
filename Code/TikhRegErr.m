73a123fa57c7327788c5bee432f4c4ccfafd6f9e
Unknown <mjbyrne2@asu.edu>
2019-01-28 11:38:23 -0700

    Changed TikhRegErr.m to handle vector inputs for lambda
    As a result, the function now contains a loop that loops over the values of lambda contained in the vector L.

--- Code/TikhRegErr.m
+++ Code/TikhRegErr.m
@@ -1,4 +1,4 @@
-function e = TikhRegErr(dataSpec,operatorSpec,smoothingSpec,lambda,...
+function e = TikhRegErr(dataSpec,operatorSpec,smoothingSpec,L,...
     trunc,origSpec)
 % TikhRegErr determines the absolute error between origSpec, which is the 
 % spectrum (DFT) of the unblurred test function, and the spectrum of
@@ -9,20 +9,26 @@ function e = TikhRegErr(dataSpec,operatorSpec,smoothingSpec,lambda,...
 % For this function to work properly, all spectrums should be zero-centered
 % (see built-in fftshift.m).
 
-filtCoeff = (conj(operatorSpec))./(abs(operatorSpec).^2 + ...
-    lambda.^2.*abs(smoothingSpec).^2);
-solSpec = filtCoeff.*dataSpec;
-
-% Truncate solution spectrum:
-N = length(solSpec);
+% Initialization:
+e = zeros(1,length(L));
+N = length(dataSpec);
 ind = (N-trunc)/2;  % Number of components to zero-out on both sides
-solSpec([1:ind,N-ind:end]) = 0;
 
-% Absolute error:
-e = err(solSpec,origSpec)*norm(origSpec);
-% e = sum((abs(original_spectrum - solution_spectrum)).^2); (original)
+for i = 1:length(e)
+    
+    filtCoeff = (conj(operatorSpec))./(abs(operatorSpec).^2 + ...
+    (L(i).^2).*abs(smoothingSpec).^2);
+    solSpec = filtCoeff.*dataSpec;
 
-% % Relative error:
-% e = err(solSpec,origSpec);
+    % Truncate solution spectrum:
+    solSpec([1:ind,N-ind:end]) = 0;
+
+    % Absolute error:
+    e(i) = err(solSpec,origSpec)*norm(origSpec);
+    % e = sum((abs(original_spectrum - solution_spectrum)).^2); (original)
+
+    % % Relative error:
+    % e = err(solSpec,origSpec);
+end
 
 end
