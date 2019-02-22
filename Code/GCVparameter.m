07c284275ad0ae01720585ab4426f371bbbbe8af
Unknown <mjbyrne2@asu.edu>
2019-02-22 12:23:57 -0700

    Changed the minimization process of the UPRE and GCV methods
    Also added a comment in MDPparameter.m.

--- Code/GCVparameter.m
+++ Code/GCVparameter.m
@@ -5,42 +5,12 @@ function [x,lambda] = GCVparameter(dataSpec,operatorSpec,smoothingSpec,...
 %
 % Companion files: GCVfunctional.m
 
-n = length(dataSpec);
-if n < 5
-    test = 0;
-else 
-    test = 1;
-end
-switch test
-    case 0
-        % Generate the UPRE vector:
-        x = GCVfunctional(dataSpec,operatorSpec,smoothingSpec,L,trunc);
+% Generate the GCV vector for plotting purposes only:
+x = GCVfunctional(dataSpec,operatorSpec,smoothingSpec,L,trunc);
 
-        % Now find the (possibily) shallow minimum:
-        step = 5;  % Step size for measuring relative increase
-        tol = 0.01; % Percent relative increase required 
-        v = (x(step:step:end)-x(1:step:end-(step-1)))./x(1:step:end-(step-1));
-        v = (x(step:step:end)-x(1:step:end-(step-1)))./...
-            (L(step:step:end)-L(1:step:end-(step-1)));
-        l1 = L((step-1)*find(abs(v) > tol,1)+1);
-
-        % Original:
-        % U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
-        %     smoothingSpec,variance,lambda,trunc);
-        % lambda = fminbnd(U,1e-6,10);
-
-        % Location of minimal component:
-        l2 = L(x == min(x));
-
-        % Take the largest parameter:
-        lambda = max(l1,l2);
-
-    case 1 
-        x = GCVfunctional(dataSpec,operatorSpec,smoothingSpec,L,trunc);
-        % Original:
-        X = @(lambda) GCVfunctional(dataSpec,operatorSpec,smoothingSpec,...
-            lambda,trunc);
-        lambda = fminbnd(X,1e-6,10);
-end   
+% Find a minimum of the function:
+G = @(lambda) GCVfunctional(dataSpec,operatorSpec,smoothingSpec,...
+    lambda,trunc);
+lambda = fminbnd(G,0,10);  
 
 end
--- Code/UPREparameter.m
+++ Code/UPREparameter.m
@@ -5,44 +5,13 @@ function [u,lambda] = UPREparameter(dataSpec,operatorSpec,smoothingSpec,...
 %
 % Companion files: UPREfunctional.m
 
-n = length(dataSpec);
-if n < 5
-    test = 0;
-else 
-    test = 1;
-end
-switch test
-    case 0
-        % Generate the UPRE vector:
-        u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
-            variance,L,trunc);
+% Generate the UPRE vector for plotting purposes only:
+u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
+    variance,L,trunc);
 
-        % Now find the (possibily) shallow minimum:
-        step = 5;  % Step size for measuring relative increase
-        tol = 0.01; % Percent relative increase required 
-        v = (u(step:step:end)-u(1:step:end-(step-1)))./u(1:step:end-(step-1));
-        v = (u(step:step:end)-u(1:step:end-(step-1)))./...
-            (L(step:step:end)-L(1:step:end-(step-1)));
-        l1 = L((step-1)*find(abs(v) > tol,1)+1);
-
-        % Original:
-        % U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
-        %     smoothingSpec,variance,lambda,trunc);
-        % lambda = fminbnd(U,1e-6,10);
-
-        % Location of minimal component:
-        l2 = L(u == min(u));
-
-        % Take the largest parameter:
-        lambda = max(l1,l2);
-
-    case 1 
-        u = UPREfunctional(dataSpec,operatorSpec,smoothingSpec,...
-            variance,L,trunc);
-        % Original:
-        U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
-            smoothingSpec,variance,lambda,trunc);
-        lambda = fminbnd(U,1e-6,10);
-end   
+% Find a minimum of the function:
+U = @(lambda) UPREfunctional(dataSpec,operatorSpec,...
+    smoothingSpec,variance,lambda,trunc);
+lambda = fminbnd(U,0,10);   
 
 end
