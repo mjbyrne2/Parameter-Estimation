ec4838751e7523420ed02b01caab3e512757e74e
mjbyrne2 <41213375+mjbyrne2@users.noreply.github.com>
2021-06-08 17:26:37 -0700

    Update AdaptiveWindowedRegularization.m
    Added some plotting commands. Last commit before reworking the implementation of the filter functions/spectrums.

--- Code/AdaptiveWindowedRegularization.m
+++ Code/AdaptiveWindowedRegularization.m
@@ -3,44 +3,41 @@
 % purpose of investigating windowed spectral Tikhonov regularization.
 
 % User inputs:
-v = 50; % Dispersion of circularly symmetric Gaussian kernel
-lSNR = 25;   % Lower bound of SNR
-rangeSNR = 0; % Range of SNR (uSNR - lSNR)
-type = 'linearCosine';    % Type of windows for regularization (see weights2.m)
-p = 2;  % Number of windows
+v = 100; % Dispersion parameter of circularly symmetric Gaussian kernel
+lSNR = 40;   % Lower bound of SNR
 penalty = 'Identity';  % Penalty matrix (Identity or Laplacian)
+type = 'log';    % Type of windows for regularization (see weights2.m)
+p = 1;  % Number of windows
+rangeSNR = 0; % Range of SNR (uSNR - lSNR)
 
 % Problem set-up:
-images = randi(15,1,2);
-% Ensure the two chosen images are distinct:
-while diff(images) == 0
-    images = randi(15,1,2);
-end
-% images = 1;
+% images = randperm(15,8);    % 8 images chosen randomly
+images = 1:8;   % Images chosen manually
 [Delta,B,X,I] = MESSENGER2(images,v,v);
-[ny,nx,R] = size(X(:,:,:));   % Extract dimension and number of images
-n = ny*nx;    % Number of total pixels in each image
-ind = randperm(R);    % Vector of shuffled indices
-X = X(:,:,ind); % Shuffle true images
-B = B(:,:,ind); % Shuffle blurred images
+[n,~,R] = size(X(:,:,:));   % Extract dimension and number of images (n = 256)
+N = n^2;    % Number of total pixels in each image (N = 65536)
+
+% Shuffle images if desired:
+% ind = randperm(R);    % Vector of shuffled indices
+% X = X(:,:,ind); % Shuffle true images
+% B = B(:,:,ind); % Shuffle blurred images
 
 % Noise constuction:
 uSNR = lSNR + rangeSNR;   % Upper bound of SNR
 % Randomly select SNR values between lSNR and uSNR:
 s = lSNR + rangeSNR.*rand(1,R);
 % Convert SNR to noise variance:
-eta = (1./(n*(10.^(s./10)))).*(arrayNorm(B).^2);
-% eta = (1./(n*(10.^(s./10)))).*(arrayNorm(X).^2);
+eta = (1./(N*(10.^(s./10)))).*(arrayNorm(B).^2);
 % Initialization of noise matrices:
-Noise = zeros(ny,nx,R);
+Noise = zeros(n,n,R);
 % Generate and add noise:
 for l = 1:R
-    Noise(:,:,l) = sqrt(eta(l))*randn(ny,nx);
+    Noise(:,:,l) = sqrt(eta(l))*randn(n);
 end
 D = B + Noise;
 
 % Compute 2D DCTs:
-D_hat = 0*D;
+D_hat = 0*D;    % Initialize D_hat
 for l = 1:R
     D_hat(:,:,l) = dct2(D(:,:,l));  % 2D DCT of D (applied to the first two dimensions of the 3D array)
 end
@@ -53,7 +50,7 @@ Dt = D(:,:,1:Rt);   % Noisy training data
 Dt_hat = D_hat(:,:,1:R);    % Noisy data in frequency domain
 sT = s(1:Rt);   % SNRs of training data
 etaT = eta(1:Rt);   % Variance of noise added to training data
-NoiseT = Noise(ny,nx,1:Rt); % Noise added to training data
+NoiseT = Noise(n,n,1:Rt); % Noise added to training data
 
 Rv = R - Rt; % Size of validation set
 Xv = X(:,:,(Rt+1):end);   % Take next Rv matrices as validation set
@@ -62,67 +59,37 @@ Dv = D(:,:,(Rt+1):end);   % Noisy validation data
 Dv_hat = D_hat(:,:,(Rt+1):end);  % Noisy validation data in frequency domain
 sV = s((Rt+1):end); % SNRs of validation data
 etaV = eta((Rt+1):end); % Variance of noise added to validation data
-NoiseV = Noise(ny,nx,(Rt+1):end);   % Noise added to validation data
+NoiseV = Noise(n,n,(Rt+1):end);   % Noise added to validation data
 
-% Define the validation set consisting of other built-in images:
-Xv2 = zeros(ny,nx,8);   % Initialization of storage array
-Xv2(:,:,1) = im2double(imread('rice.png'));  % Rice image
-I2 = im2double(imread('AT3_1m4_01.tif'));  % Cells image
-Xv2(:,:,2) = I2(end-255:end,end-255:end);  % Reshape cells image
-I3 = im2double(imread('circuit.tif'));  % Circuit image
-Xv2(:,:,3) = I3(1:256,1:256);  % Reshape circuit image
-Xv2(:,:,4) = im2double(imread('cameraman.tif'));
-I5 = im2double(imread('liftingbody.png'));  % Aircraft image
-Xv2(:,:,5) = I5(1:2:end,1:2:end);   % Downsample aircraft image
-I6 = im2double(imread('westconcordorthophoto.png'));    % Concord image
-Xv2(:,:,6) = I6(1:256,1:256);   % Reshape Concord image
-I7 = im2double(rgb2gray(imread('parkavenue.jpg'))); % Desert image
-I7 = I7(1:4:end,1:4:end);   % Downsample desert image
-Xv2(:,:,7) = I7(96:(255+96),128:(128+255)); % Reshape desert image
-I8 = im2double(rgb2gray(imread('llama.jpg'))); % Llama image
-I8 = I8(1:2:end,1:2:end);   % Downsample Llama image
-Xv2(:,:,8) = I8(65:(65+255),140:(140+255));   % Reshape llama image
-clear I2 I3 I5 I6 I7 I8
-
-% Show all extra validation images:
-BigXv2 = [Xv2(:,:,1),Xv2(:,:,2),Xv2(:,:,3),Xv2(:,:,4);...
-    Xv2(:,:,5),Xv2(:,:,6),Xv2(:,:,7),Xv2(:,:,8)];
-% imshow(BigXv2),colorbar
+% Define the validation set Xv2 consisting of 8 other built-in images:
+run Validation_Images.m
 
 % Blur the extra validation images:
 Bv2 = Xv2;  % Initialization of storage array
 for l = 1:8
    Bv2(:,:,l) = idct2(Delta.*dct2(Xv2(:,:,l))); 
 end
-BigBv2 = [Bv2(:,:,1),Bv2(:,:,2),Bv2(:,:,3),Bv2(:,:,4);...
-    Bv2(:,:,5),Bv2(:,:,6),Bv2(:,:,7),Bv2(:,:,8)];
-% imshow(BigBv2),colorbar
 
 % Create and add noise to extra validation images:
 sV2 = lSNR + rangeSNR.*rand(1,8);
-etaV2 = (1./(n*(10.^(sV2./10)))).*(arrayNorm(Bv2).^2);
-NoiseV2 = zeros(ny,nx,8);
+etaV2 = (1./(N*(10.^(sV2./10)))).*(arrayNorm(Bv2).^2);
+NoiseV2 = zeros(n,n,8);
 for l = 1:8
-    NoiseV2(:,:,l) = sqrt(etaV2(l))*randn(ny,nx);
+    NoiseV2(:,:,l) = sqrt(etaV2(l))*randn(n);
 end
 Dv2 = Bv2 + NoiseV2;
-BigDv2 = [Dv2(:,:,1),Dv2(:,:,2),Dv2(:,:,3),Dv2(:,:,4);...
-    Dv2(:,:,5),Dv2(:,:,6),Dv2(:,:,7),Dv2(:,:,8)];
-% imagesc(BigDv2),colorbar
 
-%% Regularization set-up
-
-% Penalty matrices (comment out all but one of the following Lambdas):
+% Penalty matrices:
 if isequal(penalty,'Identity')
-    Lambda = ones(ny,nx);  % DCT of l where L = I (Identity matrix)
+    Lambda = ones(n);  % DCT of l where L = I (Identity matrix)
 elseif isequal(penalty,'Laplacian') % Negative discrete Laplacian matrix
-    L = zeros(ny,nx);
-    cy = ny/2;  % Row index of stencil center
-    cx = nx/2;  % Column index of stencil center
+    L = zeros(n);
+    cy = n/2;  % Row index of stencil center
+    cx = n/2;  % Column index of stencil center
     L((cy-1):(cy+1),(cx-1):(cx+1)) = [0,-1,0;-1,4,-1;...
         0,-1,0];  % Place stencil within L
     % L((cy:cy+1),(cx:cx+1)) = [-1,1;-1,1];
-    e1 = zeros(ny,nx);
+    e1 = zeros(n);
     e1(1,1) = 1;
     Lambda = dct2(dctshift(L,[cy,cx]))./dct2(e1);
 else
@@ -130,22 +97,22 @@ else
     return
 end
 
+%% Regularization set-up
+
 % Window construction:
-% tol = eps;
-% Delta(Delta < 0) = 0;
-% Lambda(Lambda < 1) = 0;
 gamma = zeros(size(Delta));
 tol = eps;
 gamma(Lambda > tol) = Delta(Lambda > tol)./Lambda(Lambda > tol);
 % W = weights2(abs(Delta),p,type);  % Generate weights
-W = weights2(gamma,p,type);  % Generate weights
+Gamma = sqrt((conj(Delta).*Delta)./(conj(Lambda).*Lambda));
+W = weights2(Gamma,p,type);  % Generate weights
 
 switch p
    
     case 1  % p = 1 (Standard spectral regularization)
-        Gamma = @(alpha) conj(Delta)./((abs(Delta).^2) + ...
+        Gamma2 = @(alpha) conj(Delta)./((abs(Delta).^2) + ...
             (alpha*abs(Lambda).^2));
-        xWin = @(alpha,d_hat) real(idct2(Gamma(alpha).*d_hat));  % Single regularized solution
+        xWin = @(alpha,d_hat) real(idct2(Gamma2(alpha).*d_hat));  % Single regularized solution
         rWin = @(alpha,d_hat) real(idct2(Delta.*dct2(xWin(alpha,d_hat))) - ...
             idct2(d_hat));  % Single regularized residual
         Phi = @(alpha) (abs(Delta).^2)./(abs(Delta).^2 + ...
@@ -153,13 +120,13 @@ switch p
         Psi = @(alpha) 1 - Phi(alpha);
         
     otherwise  % p > 1 (Windowed spectral regularization)
-        I = sparse(repmat(eye(ny,nx),1,p)); % p-concatenation of identity matrices
-        % Function that creates an (ny x nx x p) array of diagonal matrices using I:
+        I = sparse(repmat(eye(n),1,p)); % p-concatenation of identity matrices
+        % Function that creates an (n x n x p) array of diagonal matrices using I:
         A = @(alpha) reshape(full(I*...
-            sparse(diag(reshape(repmat(alpha,ny,1),ny*p,1)))),ny,nx,p);
-        Gamma = @(alpha) conj(Delta)./((abs(Delta).^2) + ...
+            sparse(diag(reshape(repmat(alpha,n,1),n*p,1)))),n,n,p);
+        Gamma2 = @(alpha) conj(Delta)./((abs(Delta).^2) + ...
             pagemtimes(abs(Lambda).^2,A(alpha)));
-        xWin = @(alpha,d_hat) real(idct2(sum(Gamma(alpha).*d_hat.*W,3)));  % Single windowed regularized solution
+        xWin = @(alpha,d_hat) real(idct2(sum(Gamma2(alpha).*d_hat.*W,3)));  % Single windowed regularized solution
         rWin = @(alpha,d_hat) real(idct2(Delta.*dct2(xWin(alpha,d_hat))) - ...
             idct2(d_hat));  % Windowed regularized residual
         Phi = @(alpha) (abs(Delta).^2)./(abs(Delta).^2 + ...
@@ -188,22 +155,22 @@ switch p
         % UPRE:
         % UPRE = @(alpha,d_hat,eta) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) + ...
         %     (2/n)*(n*eta)*sum(sum(sum(Phi(alpha).*W,3))) - (n*eta);
-        UPRE = @(alpha,d_hat,eta) (1/n)*(norm(Psi(alpha).*d_hat,'fro')^2) + ...
-            (2/n)*(eta)*sum(sum(Phi(alpha))) - (eta);
+        UPRE = @(alpha,d_hat,eta) (1/N)*(norm(Psi(alpha).*d_hat,'fro')^2) + ...
+            (2/N)*(eta)*sum(sum(Phi(alpha))) - (eta);
         BigUPRE = @(alpha,d_hat,Eta) (1/numel(d_hat))*sum(arrayNorm(Psi(alpha).*d_hat).^2) + ...
-            (2/n)*(mean(Eta))*sum(Phi(alpha),'all') - (mean(Eta));
+            (2/N)*(mean(Eta))*sum(Phi(alpha),'all') - (mean(Eta));
 
         % GCV (no eta needed):
-        GCV = @(alpha,d_hat) (1/n)*(norm(Psi(alpha).*d_hat,'fro')^2)./...
-            ((1 - (1/n)*sum(Phi(alpha),'all')).^2);
+        GCV = @(alpha,d_hat) (1/N)*(norm(Psi(alpha).*d_hat,'fro')^2)./...
+            ((1 - (1/N)*sum(Phi(alpha),'all')).^2);
         BigGCV = @(alpha,d_hat) (1/numel(d_hat))*sum(arrayNorm(Psi(alpha).*d_hat).^2)./...
-            ((1 - (1/n)*sum(Phi(alpha),'all')).^2);
+            ((1 - (1/N)*sum(Phi(alpha),'all')).^2);
 
         % MDP:
         safeParam = 0.8;  % Only for MDP 
         % MDP = @(alpha,d_hat,eta) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - safeParam*eta*n;
         % MDP = @(alpha,d_hat,eta) ((1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - safeParam*eta*n)^2; % Squared for minimization
-        MDP = @(alpha,d_hat,eta) abs((1/n)*(norm(Psi(alpha).*d_hat,'fro')^2) - ...
+        MDP = @(alpha,d_hat,eta) abs((1/N)*(norm(Psi(alpha).*d_hat,'fro')^2) - ...
             safeParam*eta); % Absolute zero for minimization
 %         BigMDP = @(alpha,d_hat,Eta) (1/numel(d_hat))*sum(arrayNorm(Psi(alpha).*d_hat).^2) - safeParam*(mean(Eta));
         BigMDP = @(alpha,d_hat,Eta) abs((1/numel(d_hat))*sum(arrayNorm(Psi(alpha).*d_hat).^2) - ...
@@ -220,23 +187,25 @@ switch p
         % UPRE:
         % UPRE = @(alpha,d_hat,eta) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) + ...
         %     (2/n)*(n*eta)*sum(sum(sum(Phi(alpha).*W,3))) - (n*eta);
-        UPRE = @(alpha,d_hat,eta) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) + ...
-            (2/n)*(eta)*sum(Phi(alpha).*W,'all') - (eta);
+%         UPRE = @(alpha,d_hat,eta) (1/N)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) + ...
+%             (2/N)*(eta)*sum(Phi(alpha).*W,'all') - (eta);
+        UPRE = @(alpha,d_hat,eta) (1/N)*(norm(sum(Psi(alpha).*W.*d_hat,3),'fro')^2) + ...
+            (2/N)*(eta)*sum(Phi(alpha).*W,'all') - (eta);   % New
         BigUPRE = @(alpha,d_hat,Eta) (1/numel(d_hat))*sum(arrayNorm(sum(Psi(alpha).*W,3).*d_hat).^2) + ...
-            (2/n)*(mean(Eta))*sum(Phi(alpha).*W,'all') - (mean(Eta));
+            (2/N)*(mean(Eta))*sum(Phi(alpha).*W,'all') - (mean(Eta));
         
         
         % GCV (no eta needed):
-        GCV = @(alpha,d_hat) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2)./...
-            ((1 - (1/n)*sum(Phi(alpha).*W,'all')).^2);
+        GCV = @(alpha,d_hat) (1/N)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2)./...
+            ((1 - (1/N)*sum(Phi(alpha).*W,'all')).^2);
         BigGCV = @(alpha,d_hat) (1/numel(d_hat))*sum(arrayNorm(sum(Psi(alpha).*W,3).*d_hat).^2)./...
-            ((1 - (1/n)*sum(Phi(alpha).*W,'all')).^2);
+            ((1 - (1/N)*sum(Phi(alpha).*W,'all')).^2);
 
         % MDP:
         safeParam = 0.8;  % Only for MDP 
         % MDP = @(alpha,d_hat,eta) (1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - safeParam*eta*n;
         % MDP = @(alpha,d_hat,eta) ((1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - safeParam*eta*n)^2; % Squared for minimization
-        MDP = @(alpha,d_hat,eta) abs((1/n)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - ...
+        MDP = @(alpha,d_hat,eta) abs((1/N)*(norm(sum(Psi(alpha).*W,3).*d_hat,'fro')^2) - ...
             safeParam*eta); % Absolute value for minimization
         BigMDP = @(alpha,d_hat,Eta) abs((1/numel(d_hat))*sum(arrayNorm(sum(Psi(alpha).*W,3).*d_hat).^2) - ...
             safeParam*(mean(Eta)));   % Absolute value for minimization
@@ -314,7 +283,7 @@ err = [err_best,err_UPRE,err_GCV,err_MDP];              % All errors
 
 %% Find adapted regularization parameters
 
-Rvec = [1,2,4,8];   % Vector containing the number of data sets
+Rvec = 2:2:Rt;   % Vector containing the number of data sets
 r = length(Rvec);   % Number of different data sets considered in adapted methods
 
 % "Learned" storage:
@@ -366,6 +335,10 @@ for l = 1:r
     err_BigMDP(l,:) = (arrayNorm(Xv - xBigWin(alpha_BigMDP(l,:),W,Dv_hat,Delta,Lambda))./...
         arrayNorm(Xv)).^2;    % Relative error
     
+
+    disp(['p = ' num2str(p) ' parameter(s) determined using ' ...
+        num2str(r) 'data sets.'])
+
 end
 
 errBig = zeros(r,Rv,4); % 4 methods being consider; in order: MSE,UPRE,GCV,MDP
@@ -380,27 +353,91 @@ alphaBig(:,:,2) = alpha_BigUPRE;
 alphaBig(:,:,3) = alpha_BigGCV;
 alphaBig(:,:,4) = alpha_BigMDP;
 
-%% Save data
+% %% Save data
+% 
+% save(['v',num2str(v),'_','SNR',num2str(lSNR),'-',num2str(uSNR),'_',...
+%     type,num2str(p),'_',penalty],'alpha','alphaBig','err','errBig')
+% clc
+% % clear all
+% 
+% %% Plots of regularized solutions
+% 
+% for l = 1:R
+%    if l <= Rt
+%       T = ['Training set ' num2str(l)]; 
+%    else
+%       T = ['Validation set ' num2str(l-Rt)];
+%    end
+%    figure('Name',T)
+%    t = tiledlayout(2,3);
+%    nexttile,imagesc(D(:,:,l)),colorbar,title('Data'),colormap gray
+%    nexttile,imagesc(X(:,:,l)),colorbar,title('True'),colormap gray
+%    nexttile,imagesc(X_best(:,:,l)),colorbar,title('Best'),colormap gray
+%    nexttile,imagesc(X_UPRE(:,:,l)),colorbar,title('UPRE'),colormap gray
+%    nexttile,imagesc(X_GCV(:,:,l)),colorbar,title('GCV'),colormap gray
+%    nexttile,imagesc(X_MDP(:,:,l)),colorbar,title('MDP'),colormap gray
+% end
 
-save(['v',num2str(v),'_','SNR',num2str(lSNR),'-',num2str(uSNR),'_',...
-    type,num2str(p),'_',penalty],'alpha','alphaBig','err','errBig')
-clc
-% clear all
+%% Plots
 
-%% Plots of regularized solutions
+% Save a copies of images in Bv2 and Dv2 as a single images:
+BigBv2 = [Bv2(:,:,1),Bv2(:,:,2),Bv2(:,:,3),Bv2(:,:,4);...
+    Bv2(:,:,5),Bv2(:,:,6),Bv2(:,:,7),Bv2(:,:,8)];
+BigDv2 = [Dv2(:,:,1),Dv2(:,:,2),Dv2(:,:,3),Dv2(:,:,4);...
+    Dv2(:,:,5),Dv2(:,:,6),Dv2(:,:,7),Dv2(:,:,8)];
 
-for l = 1:R
-   if l <= Rt
-      T = ['Training set ' num2str(l)]; 
-   else
-      T = ['Validation set ' num2str(l-Rt)];
-   end
-   figure('Name',T)
-   t = tiledlayout(2,3);
-   nexttile,imagesc(D(:,:,l)),colorbar,title('Data'),colormap gray
-   nexttile,imagesc(X(:,:,l)),colorbar,title('True'),colormap gray
-   nexttile,imagesc(X_best(:,:,l)),colorbar,title('Best'),colormap gray
-   nexttile,imagesc(X_UPRE(:,:,l)),colorbar,title('UPRE'),colormap gray
-   nexttile,imagesc(X_GCV(:,:,l)),colorbar,title('GCV'),colormap gray
-   nexttile,imagesc(X_MDP(:,:,l)),colorbar,title('MDP'),colormap gray
-end
+WS = 'maximized';    % Window state
+LW = 2; % Line width
+MS = 12; % Marker size
+CB_FS = 20;    % Font size of colorbar labels
+Ax_FS = 18;     % Font size of axis tickmarks
+AxL_FS = 20;    % Font size of x and y axes
+L_FS = 20;  % Font size of legends
+
+% All MESSENGER images
+[~,~,~,I] = MESSENGER2(1:15);
+BigI = [I(:,:,1),I(:,:,2),I(:,:,3),I(:,:,4),I(:,:,5);...
+    I(:,:,6),I(:,:,7),I(:,:,8),I(:,:,9),I(:,:,10);
+    I(:,:,11),I(:,:,12),I(:,:,13),I(:,:,14),I(:,:,15)];
+fig = figure;
+imshow(BigI)
+fig.WindowState = WS;
+% colormap gray
+% c = colorbar;
+% c.FontSize = CB_FS;
+% axis off
+
+% Second validation set
+BigXv2 = [Xv2(:,:,1),Xv2(:,:,2),Xv2(:,:,3),Xv2(:,:,4);...
+    Xv2(:,:,5),Xv2(:,:,6),Xv2(:,:,7),Xv2(:,:,8)];
+fig = figure;
+imshow(BigXv2)
+fig.WindowState = WS;
+% colormap gray
+% c = colorbar;
+% c.FontSize = CB_FS;
+% axis off
+
+%%
+
+fig = figure;
+count = 100;
+semilogy(2:count:20000,s100I(2:count:20000),'ro','MarkerSize',MS)
+hold on
+semilogy(2:count:20000,s200I(2:count:20000),'b+','MarkerSize',MS)
+semilogy(2:count:20000,s100L(2:count:20000),'g*','MarkerSize',MS)
+semilogy(2:count:20000,s200L(2:count:20000),'kx','MarkerSize',MS)
+vline(linear100I,'k:','Linear (\nu = 100, I)')
+% vline(linear100L,'k:','Linear (\nu = 100, L)')
+vline(linear200I,'k:','Linear (\nu = 200, I)')
+% vline(linear200L,'k:','Linear (\nu = 200, L)')
+vline(log100I,'k:','Log (\nu = 100, I)')
+vline(log100L,'k:','Log (\nu = 100, L)')
+vline(log200I,'k:','Log (\nu = 200, I)')
+vline(log200L,'k:','Log (\nu = 200, L)')
+ax = gca;
+ax.FontSize = Ax_FS;
+legend('\nu = 100, I','\nu = 200, I', '\nu = 100, L',...
+    '\nu = 200, L','FontSize',L_FS)
+fig.WindowState = WS;
+xlabel('Sorted index','FontSize',AxL_FS)
