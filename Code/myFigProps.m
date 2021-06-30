7970a6726fd2b97073b3ddcbeef9607a6d09d1f7
Michael Byrne <mjbyrne2@asu.edu>
2021-06-30 00:14:33 -0700

    Added DataProcessing.m and updated myFigProps.m

--- Code/myFigProps.m
+++ Code/myFigProps.m
@@ -1,33 +1,57 @@
 % A script to make plots look good for printing and display in class notes
 % First do the plot of the data, and then run this, and then do titles and
 % so forth
-if exist('a')
-    aOld = a;
+
+% WS = 'maximized';    % Window state
+% LW = 1.5; % Line width
+% MS = 12; % Marker size
+% CB_FS = 20;    % Font size of colorbar labels (Good)
+% Ax_FS = 20;     % Font size of axis tickmarks
+% AxL_FS = 22;    % Font size of x and y axes labels
+% L_FS = 22;  % Font size of legends
+% Title_FS = 22;  % Good
+
+% Can set font sizes of "empty" objects like XLabels
+
+WS = 'maximized';    % Window state
+LW = 1.5; % Line width
+MS = 12; % Marker size
+CB_FS = 20;    % Font size of colorbar labels (Good)
+Ax_FS = 20;     % Font size of axis tickmarks
+AxL_FS = 6;    % Font size of x and y axes labels
+L_FS = 22;  % Font size of legends
+Title_FS = 22;  % Good
+
+if exist('a','var')
+    axOld = ax;
 end
-if exist('h')
-    hOld = h;
+if exist('h','var')
+    figOld = fig;
 end
 %axis tight
-axis(axis);
-h = gcf; % handles of the figure
-a = gca; % axes handle
-set(gca,'FontWeight','bold');   % This makes the text on the axis bold and the x or y label bold and the title
+% axis(axis);
+fig = gcf; % handles of the figure
+ax = gca; % axes handle
+fig.WindowState = WS; % Set the window state
+% set(gcf,'WindowState',WS) % Set the window state
+set(ax.Colorbar,'FontSize',CB_FS) % Set the colorbar font size
+% set(ax,'FontWeight','bold');   % This makes the text on the axis bold and the x or y label bold and the title
 % note that it seems to matter that we do titles etc after setting to bold
-a.TickLength = [.05,.01];
-set(a,'LineWidth',2);   % This makes the width of the axis box wider
-set(findobj('Type','line'),'MarkerSize',12)% Change the marker size of the line
-set(findobj('Type','line'),'LineWidth',2)%change the line width
+% ax.TickLength = [.05,.01];
+set(ax,'LineWidth',2);   % This makes the width of the axis box wider
+set(findobj('Type','line'),'MarkerSize',MS) % Change the marker size of the line
+set(findobj('Type','line'),'LineWidth',LW)   %change the line width
 set(findobj('Type','text'),'FontWeight','bold','FontSize',16)
 set(findobj('Type','axes'),'FontWeight','bold','FontSize',16)
 set(findobj('Type','title'),'FontWeight','bold')
-%set(findobj('Type','axes'),'FontSize',16)%increset(findobj('Type','axes'),'FontName','times')ase the size of the text for labels and title
-%set(findobj('Type','axes'),'FontName','Courier')%Roman, Helvetica, Courier,Times
+%set(findobj('Type','axes'),'FontSize',16)  %increset(findobj('Type','axes'),'FontName','times')ase the size of the text for labels and title
+%set(findobj('Type','axes'),'FontName','Courier')   %Roman, Helvetica, Courier,Times
 %set(findobj('Type','text'),'FontAngle','italic')
-%set(findobj('Type','axes'),'FontAngle','italic')%Changes everything to italic- the labels, title, axes, legend
-if exist('aold')
-    a=aOld;
+%set(findobj('Type','axes'),'FontAngle','italic')   %Changes everything to italic- the labels, title, axes, legend
+if exist('aOld','var')
+    ax = axOld;
 end
-if exist('ho')
-    h=hOld;
+if exist('hOld','var')
+    fig = figOld;
 end
 hold off
