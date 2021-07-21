9a7ff0fb8b6bc395ca791adad5db061423fd60af
Michael Byrne <mjbyrne2@asu.edu>
2021-07-20 22:41:07 -0700

    Updated visualize_dataSets.m

--- Code/visualize_dataSets.m
+++ Code/visualize_dataSets.m
@@ -26,22 +26,50 @@ imshow(BigXv2)
 myFigProps(fig)
 
 %% Plot the decay of the spectral values
-% (Must generate vectors by hand)
+% The generalized spectrum of pairs (A,L) are created for blur amounts of
+% 9 (mild) and 25 (severe) for choices of the identity and Laplacian 
+% penalty matrices for a total of 4 spectrums. Two linear and log-spaced 
+% windows were generated for each spectrum and the number of non-zero
+% elements in the first window are plotted as vertical lines.
+
+% Form the spectrums and windows:
+configs = {{9,"Identity"};{9,"Laplacian"};{25,"Identity"};...
+    {25,"Laplacian"}};
+s = zeros(4,20000); % Take the first 20,000 of 65,536 spectral components
+windowTypes = ["linear","log"];
+w = zeros(4,2); % Initialized storage for counting non-zero window elements
+
+% Loop over blur/penalty configurations:
+for j = 1:length(configs)
+    [Delta,~,~,~,~] = MESSENGER2(images,configs{j}{1},configs{j}{1});   % System matrix spectrum
+    delta = sqrt(conj(Delta).*Delta);
+    Lambda = penaltyMatrix(configs{j}{2});  % Penalty spectrum
+    lambda = sqrt(conj(Lambda).*Lambda);
+    gamma = delta./lambda;  % Generalized spectrum
+    g = sort(gamma,'descend');  % Sort spectrum in descending order 
+    s = g(1:20000); % Trim spectrum for plotting purposes
+    % Loop over window types:
+    for k = 1:length(windowTypes)
+        W = weight2(gamma,2,windowTypes(k));
+        w(j,k) = sum(W(:,:,1),'all');   % Sum of all non-zero components in the first window
+    end  
+end
+
+specConfigs = {'\nu = 100, I','\nu = 200, I', '\nu = 100, L',...
+    '\nu = 200, L'};
 
 fig = figure;
-count = 100;
-semilogy(2:count:20000,s100I(2:count:20000),'ro')
+step = 100;
+semilogy(2:step:20000,s100I(2:step:20000),'ro')
 hold on
-semilogy(2:count:20000,s200I(2:count:20000),'b+')
-semilogy(2:count:20000,s100L(2:count:20000),'g*')
-semilogy(2:count:20000,s200L(2:count:20000),'kx')
-vline(linear100I,'k:','Linear (\nu = 100, I)')
-vline(linear200I,'k:','Linear (\nu = 200, I)')
-vline(log100I,'k:','Log (\nu = 100, I)')
-vline(log100L,'k:','Log (\nu = 100, L)')
-vline(log200I,'k:','Log (\nu = 200, I)')
-vline(log200L,'k:','Log (\nu = 200, L)')
-ax = gca;
-ax.FontSize = Ax_FS;
+semilogy(2:step:20000,s200I(2:step:20000),'b+')
+semilogy(2:step:20000,s100L(2:step:20000),'g*')
+semilogy(2:step:20000,s200L(2:step:20000),'kx')
+xline(linear100I,'k:','Linear (\nu = 100, I)')
+xline(linear200I,'k:','Linear (\nu = 200, I)')
+xline(log100I,'k:','Log (\nu = 100, I)')
+xline(log100L,'k:','Log (\nu = 100, L)')
+xline(log200I,'k:','Log (\nu = 200, I)')
+xline(log200L,'k:','Log (\nu = 200, L)')
 legend('\nu = 100, I','\nu = 200, I', '\nu = 100, L','\nu = 200, L')
 xlabel('Sorted index')
